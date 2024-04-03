9ec8a118405ddbfe51a8ff93cd82e36b0d930bc1
Alberto Souza <87651902+AlbertoJSouza@users.noreply.github.com>
2024-04-03 18:50:00 +0000

    estudos

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,15 @@
 # Crie uma lista
+cursos_linkedin_learning = ['Introdução a SQL', 'Python: Formação Básica', 'Python para Ciência de Dados: Formação Básica', 'Descubra a Linguagem de Programação R', 'Fundamentos de Programação: Algoritimos']
 
 # Crie um for loop para imprimir cada elemento dessa lista
-
+for curso in cursos_linkedin_learning:
+  print(curso)
 
 # Crie um objeto iterável utilizando a função range()
 
-
 # Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+
+for item in range(4, 7):
+  print(f'{item} - Estou aprendendo python.')
+else:
+  print('Fim do loop.')
