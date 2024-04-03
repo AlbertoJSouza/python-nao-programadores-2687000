2608f17438676c1e908108262709b7702aee6916
Alberto Souza <87651902+AlbertoJSouza@users.noreply.github.com>
2024-04-03 15:14:18 +0000

    excercício condicional

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,19 +1,25 @@
 # Declare 4 variáveis do tipo numérica
-
+x = 4
+y = 30
+a = 3
+e = 66
 
 # Crie uma estrutura condicional para comparar dois números
-
-
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
-
-
 # Se a condição não for cumprida, imprima na tela uma mensagem informando que a condição é negativa e informe o número de maior valor
-
-
 # Insira outras condições na estrutura condicional usando o elif
-
-
 # Incremente a estrutura condicional já existente com expressões lógicas utilizando "and" ou "or"
-
-
 # Crie uma estrutura condicional onde mais de uma condição seja verdadeira, e use apenas a palavra reservada "if"
+
+if (x > y) or (x > a):
+  print(f'A condição foi cumprida. O número {x} é maior do que {y} ou maior do que {a}')
+elif (x == a) and (e > y) :
+  print(f'Nesse caso, verificamos que o número {x} é igual a {a} e {e} maior que {y}')
+else:
+  print(f'A condição não foi cumprida. Na verdade o número {y} é maior do que {x}')
+
+
+if y > a:
+  print(f'A condição foi cumprida. O número {y} é maior do que {a}')
+if e > x:
+  print(f'Nesse caso , verificamos que o número {e} é maior do que {x}')
